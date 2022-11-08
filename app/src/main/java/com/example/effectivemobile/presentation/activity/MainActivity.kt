3f663c82c53dd94b88881192e6416c67e4ee3534
Victoria Rydina <v.rydina@andersenlab.com>
2022-11-08 16:50:00 +0300

    added home store screen

--- app/src/main/java/com/example/effectivemobile/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/example/effectivemobile/presentation/activity/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.effectivemobile.presentation.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavController
 import androidx.navigation.NavInflater
--- main/src/main/java/com/example/effectivemobile/main/MainFragment.kt
+++ main/src/main/java/com/example/effectivemobile/main/MainFragment.kt
@@ -1,5 +1,8 @@
 package com.example.effectivemobile.main
 
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.core.view.children
 import com.example.effectivemobile.core_ui.BaseFragment
 import com.example.effectivemobile.main.databinding.FragmentMainBinding
 import com.example.effectivemobile.main.di.component.DaggerMainComponent
@@ -14,5 +17,52 @@ class MainFragment : BaseFragment<FragmentMainBinding>(
         DaggerMainComponent.factory()
             .create(requireActivity().application as MainDeps)
             .inject(this)
+    }
+
+    override fun setUi() {
+        super.setUi()
+        setUpCategory()
+        setUpTitle()
+        with(binding) {
+            categoryGroup.children.forEach { child ->
+                child.setOnClickListener {
+                    clearChecking(child)
+                }
+            }
+        }
+    }
+
+    private fun clearChecking(currentChild: View) {
+        binding.apply {
+            categoryGroup.children.forEach { child ->
+                if (child != currentChild)
+                    (child as AppCompatImageButton).isPressed = false
+            }
+        }
+    }
+
+    private fun setUpTitle(){
+        with(binding){
+            titleHotSales.titleMain.setText(R.string.hot_sales)
+            titleHotSales.buttonView.setText(R.string.see_more)
+            titleBestSeller.titleMain.setText(R.string.best_seller)
+            titleBestSeller.buttonView.setText(R.string.see_more)
+        }
+    }
+
+    private fun setUpCategory(){
+        with(binding){
+            computer.categoryTitle.setText(R.string.computer)
+            health.categoryTitle.setText(R.string.health)
+            books.categoryTitle.setText(R.string.books)
+            computerTwo.categoryTitle.setText(R.string.computer)
+            healthTwo.categoryTitle.setText(R.string.health)
+
+            computer.categoryIcon.setImageResource(R.drawable.ic_computer)
+            health.categoryIcon.setImageResource(R.drawable.ic_health)
+            books.categoryIcon.setImageResource(R.drawable.ic_books)
+            computerTwo.categoryIcon.setImageResource(R.drawable.ic_computer)
+            healthTwo.categoryIcon.setImageResource(R.drawable.ic_health)
+        }
     }
 }
